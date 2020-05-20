b0bdb7e39e7165a6400b2341c866f863f3927263
AhmadDumairi00 <62051180+AhmadDumairi00@users.noreply.github.com>
2020-05-20 13:05:22 +0300

    Add files via upload

---
+++ Paradigms.java
@@ -0,0 +1,34 @@
+package Paradigms;
+import javax.swing.*;        
+import java.util.Scanner;
+
+
+public class Paradigms {
+public static void main(String args[]) { 
+	
+	//Event-Driven : 
+	/*JFrame f=new JFrame();         
+	JButton b=new JButton("click me!");  
+	b.setBounds(130,100,100, 40);
+	f.add(b);       
+	f.setSize(400,500); 
+	f.setLayout(null); 
+	f.setVisible(true); */
+	 
+// procedural : 
+  /*  Scanner scr=new Scanner(System.in);
+	int i,number,EnteredNumber;
+	number =5; 
+	  System.out.println("Enter any Number: ");
+      EnteredNumber=scr.nextInt();
+	for (i=1;i<=number;i++) { 
+		int result; 
+		result= i*number*EnteredNumber;
+		System.out.println("the result is" + result);
+	}
+*/	
+	//object-oriented: 
+
+	
+}
+}
