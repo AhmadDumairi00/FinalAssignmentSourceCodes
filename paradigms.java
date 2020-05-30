f34f758f1c238e375fcc337278ace4dab3c72ba9
AhmadDumairi00 <62051180+AhmadDumairi00@users.noreply.github.com>
2020-05-31 01:00:49 +0300

    Add files via upload

---
+++ paradigms.java
@@ -0,0 +1,17 @@
+package evendriven;
+import javax.swing.*;
+
+import java.awt.Frame;
+import java.util.Scanner;
+
+public class paradigms {
+	public static void main(String args[]) { 
+
+	Frame f=new JFrame();         
+	JButton b=new JButton("click me!");  
+	b.setBounds(130,100,100, 40);
+	f.add(b);       
+	f.setSize(400,500); 
+	f.setLayout(null); 
+	f.setVisible(true); 
+}}
